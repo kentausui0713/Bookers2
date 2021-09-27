a76115fbf39a1df3c90da3ac5a13ae79811d4223
kentausui0713 <usuikenta100@gmail.com>
2021-09-27 12:20:46 +0000

    [fix]sidebarのユーザ改良前

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,8 +19,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
-      @books = Book.all
-      render :index
+      render :edit
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,26 +24,9 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] ="You have created book successfully."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render 'books/index'
-    end
-  end
-
-
   private
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
-  end
-
-  def book_params
-    params.require(:book).permit(:title, :body)
   end
 
 end
